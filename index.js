af92b4f199fee79f69d6223f177ae95d95def9a2
iamram2510 <iamram2510@ieee.org>
2019-05-19 23:38:51 +0530

    Refactored code, added try catch
    - refactored help log and moved it to a seperate file
    - added try catch blocks for error handling

--- index.js
+++ index.js
@@ -16,27 +16,32 @@ let dir = process.cwd()
 const args = process.argv[2];
 
 (async function () {
-  if (args.split('.').reverse()[0] === 'json') {
-    var isHtmlFileCreated = await require('./lib/createIndexhtmlFile')(args)
-
-    console.log('Html file generated please view it and if you would like any changes\nyou can change the content in config.json file')
-  } else {
-    var configContent = await require('./lib/showCLi')(args)
-
-    if (configContent) {
-      fs.writeFileSync(dir + '/config.json', configContent)
-
-      fs.unlinkSync(dir + '/question.js')
-
-      var isHtmlFileCreated = await require('./lib/createIndexhtmlFile')('config.json')
-
+  try {
+    if (args.split('.').reverse()[0] === 'json') {
+      var isHtmlFileCreated = await require('./lib/createIndexhtmlFile')(args)
+  
       console.log('Html file generated please view it and if you would like any changes\nyou can change the content in config.json file')
     } else {
-      try {
+      var configContent = await require('./lib/showCLi')(args)
+  
+      if (configContent) {
+        fs.writeFileSync(dir + '/config.json', configContent)
+  
         fs.unlinkSync(dir + '/question.js')
-      } catch (error) {
-        // this catch block will not output anuthing
+  
+        var isHtmlFileCreated = await require('./lib/createIndexhtmlFile')('config.json')
+  
+        console.log('Html file generated please view it and if you would like any changes\nyou can change the content in config.json file')
+      } else {
+        try {
+          fs.unlinkSync(dir + '/question.js')
+        } catch (error) {
+          // this catch block will not output anuthing
+        }
       }
     }
+  } catch (error) {
+    console.log(require('./lib/returnHelpLog')())
   }
+  
 })()
