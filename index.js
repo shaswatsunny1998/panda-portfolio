fef498b01401dc9a0cf207c0fce050ce1ef24e9c
Angad Sharma <30529572+L04DB4L4NC3R@users.noreply.github.com>
2019-10-01 02:08:06 +0530

    var to let [hacktoberfest]

--- index.js
+++ index.js
@@ -18,19 +18,19 @@ const args = process.argv[2];
 (async function () {
   try {
     if (args.split('.').reverse()[0] === 'json') {
-      var isHtmlFileCreated = await require('./lib/createIndexhtmlFile')(args)
+      let isHtmlFileCreated = await require('./lib/createIndexhtmlFile')(args)
       let userWantToHost = await require('./lib/askToHost')()
       console.log(userWantToHost)
       console.log('Html file generated please view it and if you would like any changes\nyou can change the content in config.json file')
     } else {
-      var configContent = await require('./lib/showCLi')(args)
+      let configContent = await require('./lib/showCLi')(args)
   
       if (configContent) {
         fs.writeFileSync(dir + '/config.json', configContent)
   
         fs.unlinkSync(dir + '/question.js')
   
-        var isHtmlFileCreated = await require('./lib/createIndexhtmlFile')('config.json')
+        let isHtmlFileCreated = await require('./lib/createIndexhtmlFile')('config.json')
         let userWantToHost = await require('./lib/askToHost').hostOrNot()
         if(userWantToHost){
           try {
